8167b9747b0ed50e4129bf1a57e9a2ea827e57ea
Raamakrishnan A <raamakrishnan1997@gmail.com>
2018-05-13 21:20:50 +0530

    Show notification to view Changelog after update

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,58 @@
 'use strict';
 
 import {workspace, window, commands, Disposable, Range, ExtensionContext,
-     TextDocument, Diagnostic, DiagnosticSeverity, DiagnosticCollection, languages} from "vscode";
+        TextDocument, Diagnostic, DiagnosticSeverity, DiagnosticCollection,
+        languages, extensions, Selection, Uri} from "vscode";
 import BaseLinter from "./linter/BaseLinter";
 import IcarusLinter from "./linter/IcarusLinter";
 import XvlogLinter from "./linter/XvlogLinter";
 
 let diagnosticCollection: DiagnosticCollection;
 var linter: BaseLinter;
-
+let extensionID: string = "mshr-h.veriloghdl";
 
 export function activate(context: ExtensionContext) {
     console.log('"verilog-hdl" is now active!');
+    checkIfUpdated(context);
     workspace.onDidChangeConfiguration(configLinter, this, context.subscriptions);
     configLinter();
 }
 
+function checkIfUpdated(context: ExtensionContext) {
+    // Get previous version
+    let prevVersion: string = context.globalState.get("version", "0.0.0");
+    let pv = prevVersion.split('.').map(Number);
+    // Get current version
+    let currVersion: string = extensions.getExtension(extensionID).packageJSON.version;
+    let cv = currVersion.split('.').map(Number);
+    // check if current version > previous version
+    if(pv[0] < cv[0])
+        showUpdatedNotif();
+    else {
+        if(pv[1] < cv[1])
+            showUpdatedNotif();
+        else {
+            if(pv[2] < cv[2])
+                showUpdatedNotif();
+        }
+    }
+    // update the value
+    context.globalState.update("version", currVersion);
+}
+
+function showUpdatedNotif() {
+    window
+        .showInformationMessage("Verilog HDL extension has been updated", "Open Changelog")
+        .then(function(){
+            // get path of CHANGELOG.md
+            let changelogPath:string = extensions.getExtension(extensionID).extensionPath + "/CHANGELOG.md";
+            let path = Uri.file(changelogPath);
+            // open
+            workspace.openTextDocument(path).then(doc => {
+                window.showTextDocument(doc);
+            });
+        });
+}
 
 function configLinter() {
     let linter_name;
