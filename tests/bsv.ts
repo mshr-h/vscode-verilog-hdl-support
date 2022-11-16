8303f27cbfac99cfce585c57ff83d71b44ee3a32
Masahiro Hiramori <mhg00g13@gmail.com>
2022-11-16 22:55:21 +0900

    update bsv test

--- tests/bsv.ts
+++ tests/bsv.ts
@@ -1,52 +1,51 @@
-import { ANTLRInputStream, CommonTokenStream } from 'antlr4ts';
-import { bsvLexer } from "../src/bsvjs/bsvLexer"
-import { bsvParser } from "../src/bsvjs/bsvParser"
-import { bsvListener } from "../src/bsvjs/bsvListener"
-
-
+import { CharStreams, CommonTokenStream } from 'antlr4ts';
+import { bsvLexer } from '../src/bsvjs/bsvLexer';
+import { bsvParser } from '../src/bsvjs/bsvParser';
+import { bsvListener } from '../src/bsvjs/bsvListener';
 
 const testFolder = 'syntaxes/bsc-lib';
 import * as fs from 'fs';
 import * as path from 'path';
 
+async function main() {
+    fs.readdir(testFolder, (err, files) => {
+        if (err) throw err;
 
-fs.readdir(testFolder, (err, files) => {
-    if(err)
-        throw err;
-        
-    files.forEach(file => {
-        console.log(file);
-        var data = fs.readFileSync(path.join(testFolder, file));
-        
-        if (err)
-            throw err;
+        files.forEach((file) => {
+            console.log(file);
+            var data: Buffer = fs.readFileSync(path.join(testFolder, file));
 
-     
+            if (err) throw err;
 
+            const chars = CharStreams.fromString(data.toString());
+            const lexer = new bsvLexer(chars);
+            const tokens = new CommonTokenStream(lexer);
+            const parser = new bsvParser(tokens);
 
-        const chars = new ANTLRInputStream(data.toString());
-        const lexer = new bsvLexer(chars);
-        const tokens = new CommonTokenStream(lexer);
-        const parser = new bsvParser(tokens);
+            class MyBsvListener implements bsvListener {}
 
-        class MyBsvListener implements bsvListener {
-    
-        }
-        
-        class MyBsvErrorListener{
-            syntaxError(recognizer, offendingSymbol, line, charPositionInLine, msg, e) {
-                console.error(msg)
-                debugger
+            class MyBsvErrorListener {
+                syntaxError(
+                    recognizer,
+                    offendingSymbol,
+                    line,
+                    charPositionInLine,
+                    msg,
+                    e
+                ) {
+                    console.error(msg);
+                    debugger;
+                }
             }
-        }
-        var listener = new MyBsvListener();
-        var errorListener = new MyBsvErrorListener();
-        
-        parser.addParseListener(listener);
-        parser.removeErrorListeners();
-        parser.addErrorListener(errorListener);
-        const tree = parser.top();
+            var listener = new MyBsvListener();
+            var errorListener = new MyBsvErrorListener();
 
-        
+            parser.addParseListener(listener);
+            parser.removeErrorListeners();
+            parser.addErrorListener(errorListener);
+            const tree = parser.top();
+        });
     });
-});
+}
+
+main();
