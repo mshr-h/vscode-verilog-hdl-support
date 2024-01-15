677cc4eb409709b960b74a2e78a9486701bb1c8f
Masahiro Hiramori <mhg00g13@gmail.com>
2024-01-15 10:04:55 +0900

    add language example

---
+++ language_examples/systemverilog/with_parameter_port_list.sv
@@ -0,0 +1,9 @@
+module with_parameter_port_list #(
+        P1,
+        localparam L2 = P1+1,
+        parameter P2)
+        ( /*port list...*/ );
+        parameter  L3 = "synonym for the localparam";
+        localparam L4 = "localparam";
+        // ...
+endmodule
