6c449fc40b0b325cd0e4e04112c7f92f8efb0260
Masahiro Hiramori <contact@mshr-h.com>
2026-05-31 22:42:43 +0900

    Fix Tcl syntax highlighting for # inside regular expressions (#590)
    Fix #551

--- language_examples/example.tcl
+++ language_examples/example.tcl
@@ -1,5 +1,24 @@
 if { [expr {$input > 10}] } {
   puts $input is greater than 10!
 }
+
+# Top-level comment
+puts "hello"; # command separator comment
+
 regexp {#} "Test string"
-puts "Ruined formatting"
+puts "Still highlighted normally"
+
+regexp {^#.*$} $line
+puts "Still highlighted after regexp"
+
+if {1} {
+  # Script-body comment
+  puts "ok"
+}
+
+switch -regexp -- $line {
+  ^#.*$ {
+    # Switch body comment
+    puts "matched"
+  }
+}
