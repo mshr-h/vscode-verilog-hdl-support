7562223aeaa723283fec5bd7093e9530f007b773
Masahiro Hiramori <contact@mshr-h.com>
2026-06-01 22:10:18 +0900

    Fix region folding markers for Verilog comments

---
+++ language_examples/verilog/region_folding.v
@@ -0,0 +1,6 @@
+module sample_coefficients;
+  // region assign coeffs_const values
+  assign coeffs_const[0] = 5'd0;
+  assign coeffs_const[1] = 5'd2;
+  // endregion assign coeffs_const values
+endmodule
